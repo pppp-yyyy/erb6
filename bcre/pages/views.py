83b2b7e42176d3f3e5a11967ef899b8e787ca16e
py <py254a@gmail.com>
2025-05-27 16:34:27 +0800

    Step 3 : pages app done

--- bcre/pages/views.py
+++ bcre/pages/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    # pages_url = reverse('pages:index')
     # return HttpResponse("<h1>Hello World                    .</h1>")
     return render(request,'pages/index.html')
 
 def about(request):
+    # pages_url = reverse('pages:about')
     # return HttpResponse("<h1>Hello World                    .</h1>")
     return render(request,'pages/about.html')
