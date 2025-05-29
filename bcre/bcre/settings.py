f9203eb5eb64ccb624fe206ae1a8825328e47f19
py <py254a@gmail.com>
2025-05-29 11:50:49 +0800

    Step 5 : install models

--- bcre/bcre/settings.py
+++ bcre/bcre/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bcredb',
+        'USER' : 'postgres',
+        'PASSWORD' : os.getenv('DB_PASSWORD'),
+        'HOST' : 'localhost'
     }
 }
 
@@ -139,3 +142,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
