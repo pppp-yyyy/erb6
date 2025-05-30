1f83ca411ae1466dcd2e846978a78e22ac47e799
py <py254a@gmail.com>
2025-05-30 16:51:35 +0800

    Step 7 : listings page

--- bcre/listings/views.py
+++ bcre/listings/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+from listings.models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    # print(listings)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {"listings" : paged_listings}
+    # context = {"name" : "somethingelse"}
+    # context = {"listings" : listings}
+    return render(request, 'listings/listings.html', context)
+    # return render(request, 'listings/listings.html', {"name" : "something"})
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = Listing.objects.get(id=listing_id)
+    context = {"listing" : listing}
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
+
+
